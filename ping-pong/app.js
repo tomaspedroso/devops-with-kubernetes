40e66b008eba1dcd9211f087f3193dec945356ea
Tomas Pedroso <tomas@ped.pt>
2026-06-21 15:13:16 +0100

    Exercise 1.9 - More services

---
+++ ping-pong/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+let counter = 0;
+
+app.get('/pingpong', (req, res) => {
+  res.send(`pong ${counter}`);
+  counter++;
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
