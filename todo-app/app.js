3d9ff2b7a292ef5b7fa6ddd05a1db62298cb3f31
Tomas Pedroso <tomas@ped.pt>
2026-06-23 23:49:12 +0100

    Exercise 1.12 - The project, step 6

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  updateImage();
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}`);
 });
+
+const updateImage = async () => {
+  if (fs.existsSync('./public/assets/image.jpg')) {
+    const { mtimeMs } = fs.statSync('./public/assets/image.jpg');
+    if (Date.now() - mtimeMs < 10 * 60 * 1000) return;
+  }
+
+  try {
+    const response = await fetch('https://picsum.photos/1200');
+    const buffer = await response.arrayBuffer();
+    fs.writeFileSync('./public/assets/image.jpg', Buffer.from(buffer));
+  } catch (err) {
+    console.error('Failed to update image:', err);
+  }
+};
